9c222a6697313adb75d775a0c12fca1a03a6f9e0
HayoBaan <info@hayobaan.nl>
2014-12-24 16:39:07 +0100

    Corrected determination of target

--- lib/Cwd/utf8.pm
+++ lib/Cwd/utf8.pm
@@ -68,9 +68,6 @@ my @EXPORT_OK = qw(chdir abs_path fast_abs_path realpath fast_realpath);
 # Holds the pointers to the original version of redefined functions
 state %_orig_functions;
 
-# Target package (i.e., the one loading this module)
-my $target_package = caller;
-
 # Current package
 my $current_package = __PACKAGE__;
 
@@ -79,6 +76,9 @@ my $original_package = $current_package;
 $original_package =~ s/::utf8$//;
 
 sub import {
+    # Target package (i.e., the one loading this module)
+    my $target_package = caller;
+
     no strict qw(refs); ## no critic (TestingAndDebugging::ProhibitNoStrict)
     no warnings qw(redefine);
 
